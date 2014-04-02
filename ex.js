3988f16240410397435eb31b94184db6072ca552
Thomas Boyt <thomas.boyt@venmo.com>
2014-04-02 14:47:35 -0400

    Clean up transpiler cruft

--- ex.js
+++ ex.js
@@ -4,10 +4,7 @@ var fs = require('fs');
 function compileToTmp(filename) {
   var in_ = fs.readFileSync(filename, 'utf8');
 
-  var out = compile(in_, 'cjs', {
-    registryName: filename,
-    dirPath: 'app'
-  });
+  var out = compile(in_, filename);
 
   fs.writeFileSync(require('path').join('./tmp', filename), out.code);
   fs.writeFileSync(require('path').join('./tmp', filename + '.map'), JSON.stringify(out.map));
--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 var esprima = require('esprima');
 
 var recast = require('recast');
+
 var types = recast.types;
 var n = recast.types.namedTypes;
 var b = recast.types.builders;
-
-var path = require('path');
 
 function visitNode(node) {
   if (n.VariableDeclaration.check(node)) {
@@ -17,20 +16,20 @@ function transform(ast) {
   return types.traverse(ast, visitNode);
 }
 
-module.exports = function transpile(source, type, opts) {
+module.exports = function transpile(source, filename) {
   var recastOptions = {
     esprima: esprima,
     tabWidth: 2,
     range: true,
-    sourceFileName: path.basename(opts.registryName, '.js') + '.js',
-    sourceMapName: path.basename(opts.registryName, '.js') + '.js.map'
+    sourceFileName: filename,
+    sourceMapName: filename + '.map'
   };
 
   var ast = recast.parse(source, recastOptions);
 
-  var p = new types.NodePath(ast);
-  var programPath = p.get("program");
+  var path = new types.NodePath(ast);
+  var programPath = path.get("program");
   transform(programPath);
 
-  return recast.print(p, recastOptions);
+  return recast.print(path, recastOptions);
 };
